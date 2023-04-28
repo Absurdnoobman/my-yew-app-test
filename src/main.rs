908f42eb8a549caa129e3030cdd96449369cbff5
Absurdnoobman <jajasamag@gmail.com>
2023-04-28 21:45:49 +0700

    fff

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,39 @@ use yew::prelude::*;
 
 #[function_component]
 fn App() -> Html{
-    let h_one : String = String::from("Hello world");
+    // Text elements
+    let title: String = String::from("Temperature Coverter");
+    let text_celsius : String = "Celsius".to_string();
+    let text_fahrenheit: String = String::from("Fahrenheit");
+    let text_kelvin: String = String::from("Kelvin");
+
+    // Input
+    let input_c : i32 = 30;
+    let str_input_c : String = input_c.to_string();
 
     html! {
-        <div>
-            <h1 name="hi" title="Hi i'm kolo"> {h_one} </h1>
+        <div style="text-align: center;">
+            <div style="background-color: DodgerBlue; padding: 5px">
+                <h1 style="color: White;"> { title } </h1>
+            </div>
+            <div class="flex-container">
+                <div class="flex-child">
+                    <h2>
+                        {text_celsius}
+                    </h2>
+                    <p> {str_input_c} </p>
+                </div>
+                <div class="flex-child">
+                    <h2>
+                        {text_fahrenheit}
+                    </h2>
+                </div>
+                <div class="flex-child">
+                    <h2>
+                        {text_kelvin}
+                    </h2>
+                </div>
+            </div>
         </div>
     }
 }
